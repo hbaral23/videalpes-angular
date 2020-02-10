3cd5722c05cf728d2fb1c284cd25935485cf6d4e
hbaral23 <baral.hugoBTSSN@gmail.com>
2020-02-10 17:03:35 +0100

    component login

--- src/app/Component/login/login.component.ts
+++ src/app/Component/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatFormFieldControl } from '@angular/material/form-field';
+import { ButtonModule } from 'primeng/primeng';
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  public profileForm: FormGroup;
+  hide = true;
 
-  constructor() { }
+  constructor() { 
+    this.profileForm = new FormGroup({
+      login: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+    }); 
+  }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    if (this.profileForm.valid) {
+       // if (this.authservice.login(this.profileForm.value.login, this.profileForm.value.password))
+    }
+  }
+  
+  clicktest(e) {
+    console.log('hide txt');
+  }
 }
