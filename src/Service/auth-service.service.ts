a548473ce0b574af1579fa8148e46615fc92ca7d
RyanBertrand1 <ryan.bertrand1@gmail.com>
2020-02-10 17:41:05 +0100

    AuthService

---
+++ src/Service/auth-service.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {environment} from '../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Router} from '@angular/router';
+
+const url = environment.url;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthServiceService {
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  login(data): Observable<any> {
+    return this.http.get(url + '/api/login_check', data);
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
+
+  setToken(token: string) {
+    localStorage.setItem('token', token);
+  }
+
+  getToken(): string {
+    return localStorage.getItem('token');
+  }
+}
