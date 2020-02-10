3db84a83d9f16a29ca7d13cf7f85ab8f71bb0934
yohan <yohan.madeira@gmail.com>
2020-02-10 17:03:21 +0100

    logic for form

--- src/app/Component/project/project.component.ts
+++ src/app/Component/project/project.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import {FormGroup, FormControl} from "@angular/forms";
+import {ProjetService} from "../../../Service/projet.service";
+import {TypeService} from "../../../Service/type.service";
+
 
 @Component({
   selector: 'app-project',
@@ -7,9 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectComponent implements OnInit {
 
-  constructor() { }
+  projectForm = new FormGroup({
+    title : new FormControl(''),
+    description: new FormControl(''),
+    persons: new FormControl(''),
+    type: new FormControl('')
+  });
+
+  type : [] = [];
+
+
+  constructor(private formBuilder: FormBuilder,private projectService : ProjetService, private typeService: TypeService) { }
 
   ngOnInit() {
+    this.typeService.get().subscribe(data => {
+        this.type = data['hydra:member'];
+    });
+  }
+
+  onSubmit(){
+    console.log(this.projectForm.value);
+    this.projectService.create(this.projectForm.value).subscribe(data =>{
+      console.log(data);
+    });
   }
 
 }
