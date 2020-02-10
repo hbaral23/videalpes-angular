24f0aefb086143123256eda46c04a1b81a7d0462
hbaral23 <baral.hugoBTSSN@gmail.com>
2020-02-10 10:09:52 +0100

    setup design/router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterialDesignModule } from './material-design/material-design.module';
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserComponent } from './Component/user/user.component';
+import { LoginComponent } from './Component/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialDesignModule,
+    PrimeNgModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
