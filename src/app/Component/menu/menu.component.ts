ca6069bc3d44670d07da192cad2bbf1a2d39e1af
hbaral23 <baral.hugoBTSSN@gmail.com>
2020-02-11 16:07:22 +0100

    updt menu

--- src/app/Component/menu/menu.component.ts
+++ src/app/Component/menu/menu.component.ts
@@ -1,33 +1,36 @@
-import { Component, OnInit, HostBinding, Input } from '@angular/core';
+import { Component, OnInit, HostBinding, Input, ChangeDetectorRef } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
+
 export class MenuComponent implements OnInit {
 
-  @Input() navItems: Array<any>;
-  @HostBinding('class.sidebar-nav') true;
-  @HostBinding('attr.role') role;
-
-  constructor() {
+  constructor(media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
   }
 
   ngOnInit() {
-
-  }
-  
-  isDivider(navItem) {
-      return !!navItem.divider
   }
 
-  isTitle(navItem) {
-      return !!navItem.title
-  }
+  mobileQuery: MediaQueryList;
 
-  isHasChild(navItem) {
-      return navItem.hasOwnProperty('children') && navItem.children.length > 0;
-  }
+  fillerNav = Array.from({length: 5}, (_, i) => `Nav Item ${i + 1}`);
 
+  fillerContent = Array.from({length: 5}, () =>
+      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`);
+
+  private _mobileQueryListener: () => void;
+
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
 }
--- src/app/Component/vote/vote.component.ts
+++ src/app/Component/vote/vote.component.ts
@@ -6,8 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vote.component.scss']
 })
 export class VoteComponent implements OnInit {
-
-  constructor() { }
+  data: any;
+  
+  constructor() { 
+    this.data = {
+      labels: ['A','B','C'],
+      datasets: [
+          {
+              data: [300, 50, 100],
+              backgroundColor: [
+                  "#FF6384",
+                  "#36A2EB",
+                  "#FFCE56"
+              ],
+              hoverBackgroundColor: [
+                  "#FF6384",
+                  "#36A2EB",
+                  "#FFCE56"
+              ]
+          }]    
+      };
+  }
 
   ngOnInit() {
   }
