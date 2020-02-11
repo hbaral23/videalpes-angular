f941ce99ab89404f4d1fc3825b3391d52a1cf47b
yohan <yohan.madeira@gmail.com>
2020-02-11 11:27:49 +0100

    remove all code for project component

--- src/app/Component/project/project.module.ts
+++ src/app/Component/project/project.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {ProjectComponent} from './project.component';
-import {ReactiveFormsModule} from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [ProjectComponent],
   imports: [
-    CommonModule,
-    ReactiveFormsModule
+    CommonModule
   ]
 })
 export class ProjectModule { }
