f57d19042c7244a846513b37aa5f0969e031bb39
hbaral23 <baral.hugoBTSSN@gmail.com>
2020-02-11 17:07:19 +0100

    vote

--- src/app/Component/vote/vote.module.ts
+++ src/app/Component/vote/vote.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChartModule } from 'primeng/chart';
+
 import {VoteComponent} from './vote.component';
 
 
