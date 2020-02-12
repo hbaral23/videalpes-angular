13520815b1302b49dfff23d977d1524a4ab6b4a2
yohan <yohan.madeira@gmail.com>
2020-02-12 12:15:47 +0100

    Adding new component GenerateQRCode and his module

---
+++ src/app/Component/generate-qrcode/generate-qrcode.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-generate-qrcode',
+  templateUrl: './generate-qrcode.component.html',
+  styleUrls: ['./generate-qrcode.component.scss']
+})
+export class GenerateQRCodeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/Component/generate-qrcode/generate-qrcode.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GenerateQRCodeComponent } from "./generate-qrcode.component";
+
+
+@NgModule({
+  declarations: [GenerateQRCodeComponent],
+  imports: [
+    CommonModule,
+  ]
+})
+export class GenerateQRCodeModule { }
