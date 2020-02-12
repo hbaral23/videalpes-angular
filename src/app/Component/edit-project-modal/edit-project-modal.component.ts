36469cd9d452831712ebed28c129ce7263fff734
yohan <yohan.madeira@gmail.com>
2020-02-12 09:58:50 +0100

    get type project on select edit project modal

--- src/app/Component/edit-project-modal/edit-project-modal.component.ts
+++ src/app/Component/edit-project-modal/edit-project-modal.component.ts
@@ -1,6 +1,7 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {FormControl, FormGroup} from "@angular/forms";
-import {MAT_DIALOG_DATA} from "@angular/material/dialog";
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {ProjetService} from "../../../Service/projet.service";
 
 @Component({
   selector: 'app-edit-project-modal',
@@ -9,10 +10,11 @@ import {MAT_DIALOG_DATA} from "@angular/material/dialog";
 })
 export class EditProjectModalComponent implements OnInit {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) { }
+  constructor(private projetService: ProjetService, private dialogRef: MatDialogRef<EditProjectModalComponent>, @Inject(MAT_DIALOG_DATA) public data: any) {
+  }
 
   projectForm = new FormGroup({
-    title : new FormControl(''),
+    title: new FormControl(''),
     description: new FormControl(''),
     persons: new FormControl(''),
     type: new FormControl('')
@@ -20,5 +22,13 @@ export class EditProjectModalComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  editProjet(id) {
+    this.dialogRef.close('données modifiées');
+    this.projetService.edit(id, this.projectForm.value).subscribe(res => {
+    });
+    location.reload();
+  }
+
 
 }
