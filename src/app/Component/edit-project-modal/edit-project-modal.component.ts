6c964d4f6db1e9e52e0e393e9e6b5841869a2fd3
yohan <yohan.madeira@gmail.com>
2020-02-15 11:46:52 +0100

    change image on project modification

--- src/app/Component/edit-project-modal/edit-project-modal.component.ts
+++ src/app/Component/edit-project-modal/edit-project-modal.component.ts
@@ -2,6 +2,7 @@ import {Component, Inject, OnInit} from '@angular/core';
 import {FormControl, FormGroup} from "@angular/forms";
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import {ProjetService} from "../../../Service/projet.service";
+import {FileService} from "../../../Service/file.service";
 
 @Component({
   selector: 'app-edit-project-modal',
@@ -10,14 +11,16 @@ import {ProjetService} from "../../../Service/projet.service";
 })
 export class EditProjectModalComponent implements OnInit {
 
-  constructor(private projetService: ProjetService, private dialogRef: MatDialogRef<EditProjectModalComponent>, @Inject(MAT_DIALOG_DATA) public data: any) {
+  constructor(private projetService: ProjetService, private dialogRef: MatDialogRef<EditProjectModalComponent>, @Inject(MAT_DIALOG_DATA) public data: any, private fileService: FileService) {
   }
 
   projectForm = new FormGroup({
     title: new FormControl(''),
     description: new FormControl(''),
     persons: new FormControl(''),
-    type: new FormControl('')
+    type: new FormControl(''),
+    imgUrl: new FormControl(''),
+    file: new FormControl('')
   });
 
   ngOnInit() {
@@ -25,6 +28,14 @@ export class EditProjectModalComponent implements OnInit {
 
   editProjet() {
     this.dialogRef.close(this.projectForm.value);
+  }
+
+  onFileSelected(files: any) {
+    console.log(files[0]);
+    let formData:FormData = new FormData();
+    formData.append("file", files[0],files[0].name);
+    this.fileService.upload(formData).subscribe();
+    this.projectForm.patchValue({imgUrl: files[0].name});
   }
 
 
